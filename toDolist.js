18b82e12c087d205ca0741f5f6e3b4ba67918957
Eguonor Oputu <oputueguonor@gmail.com>
2025-04-06 20:11:06 +0100

    added comments

--- toDolist.js
+++ toDolist.js
@@ -1,28 +1,34 @@
 function addList() {
-  let input = document.getElementById("inputs");
-  let input_value = input.value.trim();
-  let newList = document.getElementById("items");
+  let input = document.getElementById("inputs"); // get the input element
+  let input_value = input.value.trim(); //Gets the input element and its trimmed value
+  let newList = document.getElementById("items"); //Gets the unordered list element where items will be added
+
+  //If the input isn't empty, creates a new list item and adds it to the list
 
   if (input_value) {
     let li = createList(input_value);
     newList.appendChild(li);
-    input.value = "";
+    input.value = ""; //Clears the input field after adding the item
   }
 }
 
+//createList(input_value) function
 function createList(input_value) {
-  let li = document.createElement("li");
+  let li = document.createElement("li"); //Creates a new list item (li element)
 
+  //creates a check box element
   let checkBox = document.createElement("input");
   checkBox.type = "checkbox";
   checkBox.className = "checkbox";
   checkBox.onclick = function () {
-    li.classList.toggle("done");
+    li.toggle("done");
   };
 
+  //A span containing the task text
   let span = document.createElement("span");
   span.textContent = input_value;
 
+  //A remove button that deletes the item when clicked
   let removebtn = document.createElement("button");
   removebtn.textContent = "Remove";
   removebtn.className = "remove";
@@ -36,33 +42,3 @@ function createList(input_value) {
 
   return li;
 }
-
-/*addList() function:
-
-Gets the input element and its trimmed value
-
-Gets the unordered list element where items will be added
-
-If the input isn't empty, creates a new list item and adds it to the list
-
-Clears the input field after adding the item
-
-createList(input_value) function:
-
-Creates a new list item (li element)
-
-Adds three child elements to it:
-
-A checkbox that toggles a "done" class (typically used for strikethrough styling)
-
-A span containing the task text
-
-A remove button that deletes the item when clicked
-
-Key features:
-
-Items can be marked as complete (using the checkbox)
-
-Items can be removed
-
-Empty inputs are ignored (due to the trim() check) */
